a668dd141fcd72b737a179d4a62bc2da5112dee2
rohitparjane <110886486+rohitparjane@users.noreply.github.com>
2023-10-23 12:49:56 +0530

    Commit3

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/login")
+		http.csrf().disable().authorizeRequests().antMatchers("/login","/user")
 		.permitAll()
 		.antMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest().authenticated()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
